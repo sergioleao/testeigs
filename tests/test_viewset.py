963a97165708f09ec82925969c3a2abc84e74dff
Sergio Leao <s.silva.leao@gmail.com>
2023-09-24 23:38:14 +0800

    fix to dev server

--- tests/test_viewset.py
+++ tests/test_viewset.py
@@ -8,23 +8,23 @@ from rest_framework import test
 class EmployerTest(test.APITestCase):
     def test_viewset_EmployeeListViewset_with_admin_token(self):
         headers = {'Authorization': "Token b79a7b34eb29b3472c53de47113c7743c9d60e11"}
-        response = self.client.get("http://194.233.64.228:8000/api/v1/admin/employee/", headers=headers )
+        response = self.client.get("http://127.0.0.1:8000/api/v1/admin/employee/", headers=headers )
         self.assertEqual(
             response.status_code, 200
         )
     def test_viewset_EmployeeListViewset_without_token(self):
-        response = self.client.get("http://194.233.64.228:8000/api/v1/admin/employee/" )
+        response = self.client.get("http://127.0.0.1:8000/api/v1/admin/employee/" )
         self.assertEqual(
             response.status_code, 401
         )
     def test_get_viewset_EmployeeViewset_with_admin_token(self):
         headers = {'Authorization': "Token b79a7b34eb29b3472c53de47113c7743c9d60e11"}
-        response = self.client.get("http://194.233.64.228:8000/api/v1/public/employeelist/", headers=headers )
+        response = self.client.get("http://127.0.0.1:8000/api/v1/public/employeelist/", headers=headers )
         self.assertEqual(
             response.status_code, 200
         )
     def test_get_viewset_EmployeeViewset_without_token(self):
-        response = self.client.get("http://194.233.64.228:8000/api/v1/public/employeelist/" )
+        response = self.client.get("http://127.0.0.1:8000/api/v1/public/employeelist/" )
         self.assertEqual(
             response.status_code, 200
         )
